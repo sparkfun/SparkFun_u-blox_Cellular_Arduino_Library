9aade77b6e51cf7bcccea9122571029ec79ce65d
Dryw Wade <dryw.wade@sparkfun.com>
2023-11-15 11:59:53 -0700

    Add SARA-R5 and LARA-R6 class files
    Placeholders for now

---
+++ src/sfe_lara_r6.h
@@ -0,0 +1,42 @@
+#ifndef SFE_LARA_R6_LIBRARY_H
+#define SFE_LARA_R6_LIBRARY_H
+
+#include "sfe_ublox_at_commands.h"
+
+// Base LARA-R6 class
+class LARA_R6: public UBLOX_AT
+{
+
+};
+
+class LARA_R6001: public LARA_R6
+{
+
+};
+
+class LARA_R6001D: public LARA_R6
+{
+
+};
+
+class LARA_R6401: public LARA_R6
+{
+
+};
+
+class LARA_R6401D: public LARA_R6
+{
+
+};
+
+class LARA_R6801_00B: public LARA_R6
+{
+
+};
+
+class LARA_R6801D: public LARA_R6
+{
+
+};
+
+#endif
---
+++ src/sfe_sara_r5.h
@@ -0,0 +1,37 @@
+#ifndef SFE_SARA_R5_LIBRARY_H
+#define SFE_SARA_R5_LIBRARY_H
+
+#include "sfe_ublox_at_commands.h"
+
+// Base SARA-R5 class
+class SARA_R5: public UBLOX_AT
+{
+
+};
+
+class SARA_R500S: public SARA_R5
+{
+
+};
+
+class SARA_R500S_01B: public SARA_R5
+{
+
+};
+
+class SARA_R500S_61B: public SARA_R5
+{
+
+};
+
+class SARA_R510M8S_61B: public SARA_R5
+{
+
+};
+
+class SARA_R510S: public SARA_R5
+{
+
+};
+
+#endif
