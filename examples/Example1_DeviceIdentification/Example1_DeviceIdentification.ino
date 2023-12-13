14c52f3bc0645ddbea7e23da7c61b7ceaf001f1d
Dryw Wade <dryw.wade@sparkfun.com>
2023-12-13 13:03:50 -0700

    Run examples through formatter
    Clang formatter in VS Code set to Microsoft style

--- examples/Example1_DeviceIdentification/Example1_DeviceIdentification.ino
+++ examples/Example1_DeviceIdentification/Example1_DeviceIdentification.ino
@@ -22,80 +22,80 @@ UBX_CELL myModule; // This example works with all modules, so the base class can
 // LARA_R6801D myModule;
 
 // Map SIM states to more readable strings
-String simStateString[] =
-{
-  "Not present",      // 0
-  "PIN needed",       // 1
-  "PIN blocked",      // 2
-  "PUK blocked",      // 3
-  "Not operational",  // 4
-  "Restricted",       // 5
-  "Operational"       // 6
+String simStateString[] = {
+    "Not present",     // 0
+    "PIN needed",      // 1
+    "PIN blocked",     // 2
+    "PUK blocked",     // 3
+    "Not operational", // 4
+    "Restricted",      // 5
+    "Operational"      // 6
 };
 
-// processSIMstate is provided to the u-blox cellular library via a 
+// processSIMstate is provided to the u-blox cellular library via a
 // callback setter -- setSIMstateReadCallback. (See setup())
 void processSIMstate(UBX_CELL_sim_states_t state)
 {
-  Serial.println();
-  Serial.print(F("SIM state:           "));
-  Serial.print(String(state));
-  Serial.println();
+    Serial.println();
+    Serial.print(F("SIM state:           "));
+    Serial.print(String(state));
+    Serial.println();
 }
 
 void setup()
 {
-  Serial.begin(115200); // Start the serial console
+    Serial.begin(115200); // Start the serial console
 
-  // Wait for user to press key to begin
-  Serial.println(F("u-blox Cellular Example 1 - Device Identification"));
-  Serial.println(F("Press any key to begin"));
-  
-  while (!Serial.available()) // Wait for the user to press a key (send any serial character)
-    ;
-  while (Serial.available()) // Empty the serial RX buffer
-    Serial.read();
+    // Wait for user to press key to begin
+    Serial.println(F("u-blox Cellular Example 1 - Device Identification"));
+    Serial.println(F("Press any key to begin"));
 
-  Serial.println(F("Beginning..."));
+    while (!Serial.available()) // Wait for the user to press a key (send any serial character)
+        ;
+    while (Serial.available()) // Empty the serial RX buffer
+        Serial.read();
 
-  // myModule.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+    Serial.println(F("Beginning..."));
 
-  // For the MicroMod Asset Tracker, we need to invert the power pin so it pulls high instead of low
-  // Uncomment the next line if required
-  // myModule.invertPowerPin(true); 
+    // myModule.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
 
-  // Initialize the module
-  if (myModule.begin(mySerial, UBX_CELL_DEFAULT_BAUD_RATE) )
-  {
-    Serial.println(F("Module connected!"));
-  }
-  else
-  {
-    Serial.println(F("Unable to communicate with the module."));
-    Serial.println(F("Manually power-on (hold the module's On button for 3 seconds) and try again."));
-    while (1) ; // Loop forever on fail
-  }
-  Serial.println();
+    // For the MicroMod Asset Tracker, we need to invert the power pin so it pulls high instead of low
+    // Uncomment the next line if required
+    // myModule.invertPowerPin(true);
 
-  Serial.println("Manufacturer ID:     " + String(myModule.getManufacturerID()));
-  Serial.println("Model ID:            " + String(myModule.getModelID()));
-  Serial.println("Firmware Version:    " + String(myModule.getFirmwareVersion()));
-  Serial.println("Product Serial No.:  " + String(myModule.getSerialNo()));
-  Serial.println("IMEI:                " + String(myModule.getIMEI()));
-  Serial.println("IMSI:                " + String(myModule.getIMSI()));
-  Serial.println("SIM CCID:            " + String(myModule.getCCID()));
-  Serial.println("Subscriber No.:      " + String(myModule.getSubscriberNo()));
-  Serial.println("Capabilities:        " + String(myModule.getCapabilities()));
+    // Initialize the module
+    if (myModule.begin(mySerial, UBX_CELL_DEFAULT_BAUD_RATE))
+    {
+        Serial.println(F("Module connected!"));
+    }
+    else
+    {
+        Serial.println(F("Unable to communicate with the module."));
+        Serial.println(F("Manually power-on (hold the module's On button for 3 seconds) and try again."));
+        while (1)
+            ; // Loop forever on fail
+    }
+    Serial.println();
 
-  // Set a callback to return the SIM state once requested
-  myModule.setSIMstateReportCallback(&processSIMstate);
-  // Now enable SIM state reporting for states 0 to 6 (by setting the reporting mode LSb)
-  if (myModule.setSIMstateReportingMode(1) == UBX_CELL_SUCCESS)
-    Serial.println("SIM state reports requested...");
-  // You can disable the SIM staus reports again by calling assetTracker.setSIMstateReportingMode(0)
+    Serial.println("Manufacturer ID:     " + String(myModule.getManufacturerID()));
+    Serial.println("Model ID:            " + String(myModule.getModelID()));
+    Serial.println("Firmware Version:    " + String(myModule.getFirmwareVersion()));
+    Serial.println("Product Serial No.:  " + String(myModule.getSerialNo()));
+    Serial.println("IMEI:                " + String(myModule.getIMEI()));
+    Serial.println("IMSI:                " + String(myModule.getIMSI()));
+    Serial.println("SIM CCID:            " + String(myModule.getCCID()));
+    Serial.println("Subscriber No.:      " + String(myModule.getSubscriberNo()));
+    Serial.println("Capabilities:        " + String(myModule.getCapabilities()));
+
+    // Set a callback to return the SIM state once requested
+    myModule.setSIMstateReportCallback(&processSIMstate);
+    // Now enable SIM state reporting for states 0 to 6 (by setting the reporting mode LSb)
+    if (myModule.setSIMstateReportingMode(1) == UBX_CELL_SUCCESS)
+        Serial.println("SIM state reports requested...");
+    // You can disable the SIM staus reports again by calling assetTracker.setSIMstateReportingMode(0)
 }
 
 void loop()
 {
-  myModule.poll(); // Keep processing data from the module so we can extract the SIM status
+    myModule.poll(); // Keep processing data from the module so we can extract the SIM status
 }
--- examples/Example7_SendSMS/Example7_SendSMS.ino
+++ examples/Example7_SendSMS/Example7_SendSMS.ino
@@ -23,107 +23,109 @@ UBX_CELL myModule; // This example works with all modules, so the base class can
 
 void setup()
 {
-  String currentOperator = "";
+    String currentOperator = "";
 
-  Serial.begin(115200); // Start the serial console
+    Serial.begin(115200); // Start the serial console
 
-  // Wait for user to press key to begin
-  Serial.println(F("u-blox Cellular Example 7 - Send SMS"));
-  Serial.println(F("Press any key to begin"));
-  
-  while (!Serial.available()) // Wait for the user to press a key (send any serial character)
-    ;
-  while (Serial.available()) // Empty the serial RX buffer
-    Serial.read();
+    // Wait for user to press key to begin
+    Serial.println(F("u-blox Cellular Example 7 - Send SMS"));
+    Serial.println(F("Press any key to begin"));
 
-  Serial.println(F("Beginning..."));
+    while (!Serial.available()) // Wait for the user to press a key (send any serial character)
+        ;
+    while (Serial.available()) // Empty the serial RX buffer
+        Serial.read();
 
-  // myModule.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
+    Serial.println(F("Beginning..."));
 
-  // For the MicroMod Asset Tracker, we need to invert the power pin so it pulls high instead of low
-  // Uncomment the next line if required
-  // myModule.invertPowerPin(true); 
+    // myModule.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
 
-  // Initialize the module
-  if (myModule.begin(mySerial, UBX_CELL_DEFAULT_BAUD_RATE) )
-  {
-    Serial.println(F("Module connected!"));
-  }
-  else
-  {
-    Serial.println(F("Unable to communicate with the module."));
-    Serial.println(F("Manually power-on (hold the module's On button for 3 seconds) and try again."));
-    while (1) ; // Loop forever on fail
-  }
-  Serial.println();
+    // For the MicroMod Asset Tracker, we need to invert the power pin so it pulls high instead of low
+    // Uncomment the next line if required
+    // myModule.invertPowerPin(true);
 
-  // First check to see if we're connected to an operator:
-  if (myModule.getOperator(&currentOperator) == UBX_CELL_SUCCESS)
-  {
-    Serial.print(F("Connected to: "));
-    Serial.println(currentOperator);
-  }
-  else
-  {
-    Serial.print(F("The module is not yet connected to an operator. Please use the previous examples to connect. Or wait and retry. Freezing..."));
-    while (1)
-      ; // Do nothing more
-  }
+    // Initialize the module
+    if (myModule.begin(mySerial, UBX_CELL_DEFAULT_BAUD_RATE))
+    {
+        Serial.println(F("Module connected!"));
+    }
+    else
+    {
+        Serial.println(F("Unable to communicate with the module."));
+        Serial.println(F("Manually power-on (hold the module's On button for 3 seconds) and try again."));
+        while (1)
+            ; // Loop forever on fail
+    }
+    Serial.println();
 
-  Serial.println();
-  Serial.println(F("*** Set the Serial Monitor line ending to Newline ***"));
+    // First check to see if we're connected to an operator:
+    if (myModule.getOperator(&currentOperator) == UBX_CELL_SUCCESS)
+    {
+        Serial.print(F("Connected to: "));
+        Serial.println(currentOperator);
+    }
+    else
+    {
+        Serial.print(F("The module is not yet connected to an operator. Please use the previous examples to connect. "
+                       "Or wait and retry. Freezing..."));
+        while (1)
+            ; // Do nothing more
+    }
+
+    Serial.println();
+    Serial.println(F("*** Set the Serial Monitor line ending to Newline ***"));
 }
 
 void loop()
 {
-  String destinationNumber = "";
-  String message = "";
-  boolean keepGoing = true;
-  
-  Serial.println();
-  Serial.println(F("Enter the destination number (followed by LF / Newline): "));
-  
-  while (keepGoing)
-  {
-    if (Serial.available())
-    {
-      char c = Serial.read();
-      if (c == '\n')
-      {
-        keepGoing = false; // Stop if we receive a newline
-      }
-      else
-      {
-        destinationNumber += c; // Add serial characters to the destination number
-      }
-    }
-  }
-  
-  keepGoing = true;
-  Serial.println();
-  Serial.println(F("Enter the message (followed by LF): "));
-  
-  while (keepGoing)
-  {
-    if (Serial.available())
-    {
-      char c = Serial.read();
-      if (c == '\n')
-      {
-        keepGoing = false; // Stop if we receive a newline
-      }
-      else
-      {
-        message += c; // Add serial characters to the destination number
-      }
-    }
-  }
+    String destinationNumber = "";
+    String message = "";
+    boolean keepGoing = true;
 
-  // Once we receive a newline, send the text.
-  Serial.println("Sending: \"" + message + "\" to " + destinationNumber);
-  // Call myModule.sendSMS(String number, String message) to send an SMS message.
-  if (myModule.sendSMS(destinationNumber, message) == UBX_CELL_SUCCESS)
-  {
-    Serial.println(F("sendSMS was successful"));
-  }
+    Serial.println();
+    Serial.println(F("Enter the destination number (followed by LF / Newline): "));
+
+    while (keepGoing)
+    {
+        if (Serial.available())
+        {
+            char c = Serial.read();
+            if (c == '\n')
+            {
+                keepGoing = false; // Stop if we receive a newline
+            }
+            else
+            {
+                destinationNumber += c; // Add serial characters to the destination number
+            }
+        }
+    }
+
+    keepGoing = true;
+    Serial.println();
+    Serial.println(F("Enter the message (followed by LF): "));
+
+    while (keepGoing)
+    {
+        if (Serial.available())
+        {
+            char c = Serial.read();
+            if (c == '\n')
+            {
+                keepGoing = false; // Stop if we receive a newline
+            }
+            else
+            {
+                message += c; // Add serial characters to the destination number
+            }
+        }
+    }
+
+    // Once we receive a newline, send the text.
+    Serial.println("Sending: \"" + message + "\" to " + destinationNumber);
+    // Call myModule.sendSMS(String number, String message) to send an SMS message.
+    if (myModule.sendSMS(destinationNumber, message) == UBX_CELL_SUCCESS)
+    {
+        Serial.println(F("sendSMS was successful"));
+    }
 }
